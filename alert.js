27d95d72250e70cd6a64c2aa7881e25a567ef6fe
front1@oscorp.pro <front1@oscorp.pro>
2020-12-10 09:55:35 +0200

    data types

--- alert.js
+++ alert.js
@@ -1,1 +1,29 @@
-[1, 2].forEach(alert)
+// [1, 2].forEach(alert)
+
+// let admin, name;
+let a, b, c;
+c = 2 + 2;
+b = c;
+a = c;
+console.log(c+''+b+''+a);
+
+c = 6;
+b = c;
+a = c;
+console.log(c+''+b+''+a);
+
+"" + 1 + 0 //10!
+"" - 1 + 0 //-1!
+true + false //1!
+6 / "3" //2!
+"2" * "3" //6!
+4 + 5 + "px" //9px!
+"$" + 4 + 5 //$45!
+"4" - 2 //2!
+"4px" - 2 // 2??? Nan
+7 / 0 //Infinity!
+"  -9  " + 5 //-95!?? - 9 5
+"  -9  " - 5 //-14!
+null + 1//Nan??? 1
+undefined + 1//nan!
+" \t \n" - 2 //-2!
