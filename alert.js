e0c01c7468e0f8e41e69a0cf283a14969d5732f5
front1@oscorp.pro <front1@oscorp.pro>
2020-12-11 14:10:03 +0200

    else...if

--- alert.js
+++ alert.js
@@ -12,6 +12,8 @@ b = c;
 a = c;
 console.log(c+''+b+''+a);
 
+
+// DATA TYPES
 "" + 1 + 0 //10!
 "" - 1 + 0 //-1!
 true + false //1!
@@ -27,3 +29,46 @@ true + false //1!
 null + 1//Nan??? 1
 undefined + 1//nan!
 " \t \n" - 2 //-2!
+
+let a1 = prompt("First number?", 1);
+let b1 = prompt("Second number?", 2);
+console.log(+a1 + +b1); // 12
+// DATA TYPES END
+
+//COMPARISON
+5 > 4                   //true
+"apple" > "pineapple"   //false
+"2" > "12"              //true
+undefined == null       //true
+undefined === null      //false
+null == "\n0\n"         //false null only equals undefined
+null === +"\n0\n"       //false
+//COMPARISON END
+
+//IF...ELSE
+let result;
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+
+let resultNew = (a + b < 4)?'Below':'Over';
+
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+
+let message= (login == 'Employee') ? 'Hello':
+    (login == 'Director') ? 'Greetings':
+    (login == '') ? 'No login':
+    '';
